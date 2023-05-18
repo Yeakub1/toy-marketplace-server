a80ce1178fa6f8cf2e22cb9be6366311342f09f8
Yeakub1 <mryeakub10@gmail.com>
2023-05-18 21:52:32 +0600

    server is start

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Toy Server is Running');
+})
+
+app.listen(port, () => {
+    console.log(`Toy Server Port On ${port}`);
+})
